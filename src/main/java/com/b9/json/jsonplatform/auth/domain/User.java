a950aa8ce0744bb3c7b3d6b8768a5423438b89ec
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-04-14 21:00:36 +0700

    feat: secure password using BCrypt and enforce unique user constraints

--- src/main/java/com/b9/json/jsonplatform/auth/domain/User.java
+++ src/main/java/com/b9/json/jsonplatform/auth/domain/User.java
@@ -11,9 +11,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
     private String fullName;
     private String role = "TITIPERS";
+    private String phoneNumber;
+    private String address;
 }
