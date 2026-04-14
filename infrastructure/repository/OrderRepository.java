c1b8648e10bf2d8b80e18a36a7705f884f1cb74e
namarabigail <bigelnp10@gmail.com>
2026-04-14 13:18:30 +0700

    refactor(order): update findByTitiperId to use UUID

--- infrastructure/repository/OrderRepository.java
+++ infrastructure/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByTitiperId(Long titiperId);
+    List<Order> findByTitiperId(UUID titiperId);
 }
