5dc814b8e6df8bfab0e4ce0818fdea535640cf25
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-04-17 14:51:03 +0700

    fix: Changing ID to UUID

--- infrastructure/repository/OrderRepository.java
+++ infrastructure/repository/OrderRepository.java
@@ -4,8 +4,9 @@ import com.b9.json.jsonplatform.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByTitiperId(Long titiperId);
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findByTitiperId(UUID titiperId);
 }
