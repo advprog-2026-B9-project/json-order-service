cfa619e28569f87a8e0405441a512b627a7df344
namarabigail <bigelnp10@gmail.com>
2026-05-08 15:17:53 +0700

    feat: add method getTotalSuccessfulOrdersByJastiper for Profile integration

--- infrastructure/repository/OrderRepository.java
+++ infrastructure/repository/OrderRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     List<Order> findByTitiperId(UUID titiperId);
+    long countByJastiperIdAndStatus(UUID jastiperId, String status);
 }
