e3ea98c5a8afad5534291c436680b61c686425e7
namarabigail <bigelnp10@gmail.com>
2026-05-21 19:48:38 +0700

    fix: delete jsonplatformapplication

---
+++ src/main/java/com/b9/json/jsonplatform/order/OrderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.b9.json.jsonplatform.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
