896bd65fa060aec90ce654484632152642548fcf
namarabigail <bigelnp10@gmail.com>
2026-05-21 22:48:40 +0700

    fix: remove duplicate OrderApplication and add ComponentScan

--- src/main/java/com/b9/json/jsonplatform/order/OrderServiceApplication.java
+++ src/main/java/com/b9/json/jsonplatform/order/OrderServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.b9.json.jsonplatform")
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
