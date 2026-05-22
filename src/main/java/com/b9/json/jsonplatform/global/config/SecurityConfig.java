d72aa7760e5a1f21694004c2f019126e643cb192
namarabigail <bigelnp10@gmail.com>
2026-05-22 21:37:57 +0700

    add cors

--- src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
+++ src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
@@ -1,25 +1,11 @@
-package com.b9.json.jsonplatform.global.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/orders/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
-    }
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/v1/orders/**").permitAll()
+                    .anyRequest().authenticated()
+            );
+    return http.build();
 }
