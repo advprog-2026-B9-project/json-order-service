636b01fec28be9ae1974d5e111d8aa7d8859234b
namarabigail <bigelnp10@gmail.com>
2026-05-22 21:26:10 +0700

    change vercel path

--- src/main/java/com/b9/json/jsonplatform/global/config/CorsConfig.java
+++ src/main/java/com/b9/json/jsonplatform/global/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("http://localhost:3000",
                 "http://localhost:8080",
                 "https://json-backend-b09.taila1e039.ts.net",
-                "https://json-frontend-two.vercel.app"));
+                "https://json-b9.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
+++ src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/orders/**").permitAll()
